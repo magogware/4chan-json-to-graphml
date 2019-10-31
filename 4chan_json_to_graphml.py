8e35fe4857f7e242f2b81a0c88d434c0072d5a03
Alex Barilaro <a_barilaro@pm.me>
2019-10-31 16:24:20 +1100

    Added comment collection

--- 4chan_json_to_graphml.py
+++ 4chan_json_to_graphml.py
@@ -44,16 +44,23 @@ def json_to_graphml(data, filename):
         
         # Create a regular expression to catch
         # replies in a post's text
-        exp = "(\\#p\\d+)"
+        quote = re.compile("(&gt;&gt;\\d+)")
 
         # Create a counter to keep track of
         # the current node id
         i = 0
         
         # Print out XML preamble
-        graph_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">")
-        graph_file.write("<key id=\"g_type\" for=\"graph\" attr.name=\"type\" attr.type=\"string\"/><key id=\"v_name\" for=\"node\" attr.name=\"name\" attr.type=\"string\"/>")
-        graph_file.write("<graph id=\"G\" edgedefault=\"directed\"><data key=\"g_type\">www-hyperlink</data>")
+        graph_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">\n")
+        graph_file.write("<key id=\"g_type\" for=\"graph\" attr.name=\"type\" attr.type=\"string\"/>\n")
+        graph_file.write("<key id=\"v_name\" for=\"node\" attr.name=\"name\" attr.type=\"string\"/>\n")
+        graph_file.write("<key id=\"v_comment\" for=\"node\" attr.name=\"comment\" attr.type=\"string\"/>\n")
+        graph_file.write("<graph id=\"G\" edgedefault=\"directed\"><data key=\"g_type\">www-hyperlink</data>\n")
+
+        span = re.compile("(<span .*>.*<\\s*\\/span>)")
+        symb = re.compile("(&(\\w+;)?)")
+        a = re.compile("(<a .*>.*<\\s*\\/a>)")
+        br = re.compile("(<br\\s*\\/?>)")
 
         # Loop through all posts in a thread
         for post in data['posts']:
@@ -64,9 +71,11 @@ def json_to_graphml(data, filename):
 
             # Print the node XML, using the
             # post number as the name
-            graph_file.write("<node id=\""+node_id+"\">")
-            graph_file.write("<data key=\"v_name\">"+str(post['no'])+"</data>")
-            graph_file.write("</node>")
+            graph_file.write("<node id=\""+node_id+"\">\n")
+            graph_file.write("\t<data key=\"v_name\">"+str(post['no'])+"</data>\n")
+            sanitised_comment = symb.sub("", br.sub("", a.sub("", span.sub("", post['com'])))).replace("&", "").replace(">", "")
+            graph_file.write("\t<data key=\"v_comment\">"+sanitised_comment+"</data>\n")
+            graph_file.write("</node>\n")
 
             # If the post has text, parse it for
             # replies
@@ -74,13 +83,13 @@ def json_to_graphml(data, filename):
 
                 # Use the regex to catch replies
                 text = post['com']
-                replies_to = re.findall(exp, text)
+                replies_to = quote.findall(text)
 
                 # Loop through all replies gathered
                 for reply in replies_to:
 
                     # Clean extra characters off reply
-                    reply = reply.replace("#p", "")
+                    reply = reply.replace("&gt;&gt;", "")
 
                     # If we have information on the reply...
                     if reply in d.keys():
@@ -92,13 +101,13 @@ def json_to_graphml(data, filename):
                         # Construct the edge XML and print it
                         edge_xml  = "<edge source=\""+node_id+"\" target=\""+replied_to+"\">"
                         graph_file.write(edge_xml)
-                        graph_file.write("</edge>")
+                        graph_file.write("</edge>\n")
 
             # Increment the node ID counter
             i = i + 1
 
         # Close off the GraphML
-        graph_file.write("</graph></graphml>")
+        graph_file.write("</graph>\n</graphml>")
 
 def desu_to_graphml(data, filename):
 
@@ -112,7 +121,7 @@ def desu_to_graphml(data, filename):
         
         # Create a regular expression to catch
         # replies in a post's text
-        exp = "(\\>>\\d+)"
+        quote = re.compile("(>>\\d+)")
 
         # Create a counter to keep track of
         # the current node id
@@ -120,8 +129,10 @@ def desu_to_graphml(data, filename):
         
         # Print out XML preamble
         graph_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">")
-        graph_file.write("<key id=\"g_type\" for=\"graph\" attr.name=\"type\" attr.type=\"string\"/><key id=\"v_name\" for=\"node\" attr.name=\"name\" attr.type=\"string\"/>")
-        graph_file.write("<graph id=\"G\" edgedefault=\"directed\"><data key=\"g_type\">www-hyperlink</data>")
+        graph_file.write("<key id=\"g_type\" for=\"graph\" attr.name=\"type\" attr.type=\"string\"/>\n")
+        graph_file.write("<key id=\"v_name\" for=\"node\" attr.name=\"name\" attr.type=\"string\"/>\n")
+        graph_file.write("<key id=\"v_comment\" for=\"node\" attr.name=\"comment\" attr.type=\"string\"/>\n")
+        graph_file.write("<graph id=\"G\" edgedefault=\"directed\"><data key=\"g_type\">www-hyperlink</data>\n")
 
         # Store the OP as node 0
         post = data['op']
@@ -132,9 +143,11 @@ def desu_to_graphml(data, filename):
 
         # Print the node XML, using the
         # post number as the name
-        graph_file.write("<node id=\""+node_id+"\">")
-        graph_file.write("<data key=\"v_name\">"+str(post['num'])+"</data>")
-        graph_file.write("</node>")
+        graph_file.write("<node id=\""+node_id+"\">\n")
+        graph_file.write("\t<data key=\"v_name\">"+str(post['num'])+"</data>\n")
+        sanitised_comment = str(post['comment']).replace(">", "").replace("&", "").replace("\n", " ")
+        graph_file.write("\t<data key=\"v_comment\">"+sanitised_comment+"</data>\n")
+        graph_file.write("</node>\n")
 
         # Increment the counter
         i = i + 1
@@ -151,9 +164,11 @@ def desu_to_graphml(data, filename):
 
             # Print the node XML, using the
             # post number as the name
-            graph_file.write("<node id=\""+node_id+"\">")
-            graph_file.write("<data key=\"v_name\">"+str(post['num'])+"</data>")
-            graph_file.write("</node>")
+            graph_file.write("<node id=\""+node_id+"\">\n")
+            graph_file.write("\t<data key=\"v_name\">"+str(post['num'])+"</data>\n")
+            sanitised_comment = str(post['comment']).replace(">", "").replace("&", "").replace("\n", " ")
+            graph_file.write("\t<data key=\"v_comment\">"+sanitised_comment+"</data>\n")
+            graph_file.write("</node>\n")
 
             # If the post has text, parse it for
             # replies
@@ -161,7 +176,7 @@ def desu_to_graphml(data, filename):
 
                 # Use the regex to catch replies
                 text = post['comment']
-                replies_to = re.findall(exp, text)
+                replies_to = quote.findall(text)
 
                 # Loop through all replies gathered
                 for reply in replies_to:
@@ -179,13 +194,13 @@ def desu_to_graphml(data, filename):
                         # Construct the edge XML and print it
                         edge_xml  = "<edge source=\""+node_id+"\" target=\""+replied_to+"\">"
                         graph_file.write(edge_xml)
-                        graph_file.write("</edge>")
+                        graph_file.write("</edge>\n")
 
             # Increment the node ID counter
             i = i + 1
 
         # Close off the GraphML
-        graph_file.write("</graph></graphml>")
+        graph_file.write("</graph>\n</graphml>")
 
 #################
 ## Main function
